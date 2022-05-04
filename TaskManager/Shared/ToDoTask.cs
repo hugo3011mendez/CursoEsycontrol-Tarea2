02b051d8c77958b98b5a04969812f0b15dc4d88f
hugo3011mendez <hugo3011mendez@gmail.com>
2022-05-04 10:39:18 +0200

    Creada clase para las tareas en el proyecto de Shared y se han realizado los enlaces pertinentes para usarla en el Index.
    Instalado Microsoft.EntityFrameworkCore en una versión soportable por NET5.0.

---
+++ TaskManager/Shared/ToDoTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Shared
+{
+    public class ToDoTask
+    {
+		[Key]
+		public Guid Id { get; set; }
+
+		// Propiedades de las tareas
+		public string TaskName { get; set; }
+		public string DescriptionTask { get; set; }
+		public bool Done { get; set; }
+		public int TaskID { get; set; }
+
+		// Constructor para crear una nueva tarea
+		public ToDoTask(string name, string desc)
+		{
+			//TaskID = countTaskId;
+			//countTaskId++;
+			Done = false;
+
+			TaskName = name;
+			DescriptionTask = desc;
+		}
+	}
+}
